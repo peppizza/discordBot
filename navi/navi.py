493d019c5a745bbfaf6a778b16aa6218ceb8f28a
peppizza <spencervess@gmail.com>
2020-04-30 10:43:02 -0400

    added rules command

--- navi/navi.py
+++ navi/navi.py
@@ -1,4 +1,15 @@
 # 'HEY' 'LOOK" "LISTEN" "HEY LISTEN"
+#Rules:
+#-Hate speech is not allowed.
+#-Nothing rude or demeaning should be said to someone, unless the person in question is okay with it. (for instance friends insulting each other back and forth)
+#-Refrain from sharing personal information, especially someone else's!
+#-Don't harass people. 
+#-Don't spam
+#-If a mod or admin says not to do something, then you must not do it.
+#-If a mod is abusing their power then dm !report to @fellow_kids 
+#-Images that may contain gore or other graphic content that is not safe for work are not allowed
+#-Threats are not allowed, if for some reason its a joke, then make it clear that there is no ill intentions. (You don't really have to worry about this one most of the time, as it will only be dealt with if a mod or admin sees fit)
+#-Self promotion is okay as long as it remains in its respective channel
 
 import os
 import secrets
@@ -15,6 +26,12 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def rules(ctx):
+    channel = bot.get_channel(704737000078704710)
+    embed = discord.Embed(title='Rules', description='-Hate speech is not allowed.\n-Nothing rude or demeaning should be said to someone, unless the person in question is okay with it. (for instance friends insulting each other back and forth)\n-Refrain from sharing personal information, especially someone else\'s!\n-Don\'t harass people. \n-Don\'t spam\n-If a mod or admin says not to do something, then you must not do it.\n-If a mod is abusing their power then dm !report to @fellow_kids \n-Images that may contain gore or other graphic content that is not safe for work are not allowed\n-Threats are not allowed, if for some reason its a joke, then make it clear that there is no ill intentions. (You don\'t really have to worry about this one most of the time, as it will only be dealt with if a mod or admin sees fit)\n-Self promotion is okay as long as it remains in its respective channel')
+    await channel.send(embed=embed)
 
 @bot.command(name='start')
 @commands.has_role('pinging rights')
