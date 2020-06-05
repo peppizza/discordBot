536dde239c47b256a83158c3314ac690dd934048
Shock95x <xshockinfirex@gmail.com>
2020-06-05 19:11:42 -0400

    small edit

--- fellow_kids/commandFiles/suggestion_handler.py
+++ fellow_kids/commandFiles/suggestion_handler.py
@@ -3,7 +3,7 @@ import random
 
 from discord.ext import commands
 
-from commandFiles import THIS_FOLDER, ROLE_MODERATOR, CATEGORY_OPENSUGGESTIONS, CATEGORY_CLOSEDSUGGESTIONS, ROLE_EVERYONE, ROLE_BOTDEVELOPER
+from commandFiles import THIS_FOLDER, ROLE_MODERATOR, CATEGORY_OPENSUGGESTIONS, CATEGORY_CLOSEDSUGGESTIONS, ROLE_EVERYONE, ROLE_BOTDEVELOPER, ROLE_ADMINISTRATOR, ROLE_BOTS
 
 suggestions = [697102775959552052, 702882611256754289, 702882635365613662, 703312812096749599, 703312842715037766, 703312883055984730, 703312953637863515, 703313003889688696, 703313116154560583]
 
@@ -31,7 +31,8 @@ class SuggestionHandler(commands.Cog):
         if message.channel.id in suggestions:
             channel = message.channel
             catagory = channel.category
-            if catagory.id == CATEGORY_OPENSUGGESTIONS and message.author.id != 681886537046163506 and message.author.id != 253290704384557057 and message.author.id != 704350265590939649 and not message.author.id in self.bannedusers:
+            sender = message.author
+            if catagory.id == CATEGORY_OPENSUGGESTIONS and not sender.has_role(ROLE_ADMINISTRATOR) and not sender.has_role(ROLE_BOTS) and not sender.id in self.bannedusers:
                 embed = discord.Embed(title='working on..', description='Your request is now being worked on by the devs', color=0x00ff00)
                 category = self.bot.get_channel(CATEGORY_CLOSEDSUGGESTIONS)
                 guild = message.guild
@@ -43,14 +44,13 @@ class SuggestionHandler(commands.Cog):
                 await channel.set_permissions(dev, send_messages=True)    
 
                 await channel.send(dev.mention, embed=embed)
-                #await channel.send('<@!{}>'.format(253290704384557057), embed=embed)
                 await self.inactivetoopen()
 
     @commands.command(aliases=['done'])
     async def close(self, ctx):
         channel = ctx.message.channel
         category = self.bot.get_channel(703314675529416785)
-        embed = discord.Embed(title='inactive', description='this channel is now inactive', color=0x000000)
+        embed = discord.Embed(title='inactive', description='This channel is now inactive', color=0x000000)
         await channel.edit(name=str(channel.name).replace('⌛', ''), category=category, sync_permissions=True)
         await ctx.send(embed=embed)
     
