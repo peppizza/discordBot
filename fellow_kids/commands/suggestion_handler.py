47228f5208e82a244078cfdcd0de0eb3370cc5a1
peppizza <spencervess@gmail.com>
2020-06-06 23:46:14 -0400

    removed dumb things that make me feel bad

--- fellow_kids/commands/suggestion_handler.py
+++ fellow_kids/commands/suggestion_handler.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 
 from .constants import ROLE_MODERATOR, CATEGORY_OPENSUGGESTIONS, CATEGORY_CLOSEDSUGGESTIONS, ROLE_EVERYONE, ROLE_BOTDEVELOPER, ROLE_ADMINISTRATOR
 
-suggestions = [697102775959552052, 702882611256754289, 702882635365613662, 703312812096749599, 703312842715037766, 703312883055984730, 703312953637863515, 703313003889688696, 703313116154560583]
 
 class SuggestionHandler(commands.Cog):
 
@@ -29,23 +28,22 @@ class SuggestionHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id in suggestions:
-            channel = message.channel
-            catagory = channel.category
-            sender = message.author
-            if catagory.id == CATEGORY_OPENSUGGESTIONS and not ROLE_ADMINISTRATOR in sender.roles and not sender.bot and not sender.id in self.bannedusers:
-                embed = discord.Embed(title='working on..', description='Your request is now being worked on by the devs', color=0x00ff00)
-                category = self.bot.get_channel(CATEGORY_CLOSEDSUGGESTIONS)
-                guild = message.guild
-                everyone = guild.get_role(ROLE_EVERYONE)
-                dev = guild.get_role(ROLE_BOTDEVELOPER)
-                await channel.edit(name='{}-⌛'.format((message.channel.name).replace('✅', '')), category=category)
-                await channel.set_permissions(message.author, send_messages=True)
-                await channel.set_permissions(everyone, send_messages=False)
-                await channel.set_permissions(dev, send_messages=True)
+        channel = message.channel
+        catagory = channel.category
+        sender = message.author
+        if catagory.id == CATEGORY_OPENSUGGESTIONS and not ROLE_ADMINISTRATOR == sender.top_role.id and not sender.bot and not sender.id in self.bannedusers:
+            embed = discord.Embed(title='working on..', description='Your request is now being worked on by the devs', color=0x00ff00)
+            category = self.bot.get_channel(CATEGORY_CLOSEDSUGGESTIONS)
+            guild = message.guild
+            everyone = guild.get_role(ROLE_EVERYONE)
+            dev = guild.get_role(ROLE_BOTDEVELOPER)
+            await channel.edit(name='{}-⌛'.format((message.channel.name).replace('✅', '')), category=category)
+            await channel.set_permissions(message.author, send_messages=True)
+            await channel.set_permissions(everyone, send_messages=False)
+            await channel.set_permissions(dev, send_messages=True)
 
-                await channel.send(dev.mention, embed=embed)
-                await self.inactivetoopen()
+            await channel.send(dev.mention, embed=embed)
+            await self.inactivetoopen()
 
     @commands.command(aliases=['done'])
     async def close(self, ctx):
