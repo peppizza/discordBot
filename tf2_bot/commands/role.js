d4f8a43f918c1a1f149b94b8a840bbed033cddaa
peppizza <spencervess@gmail.com>
2020-07-04 15:03:25 -0400

    remove old tf2_bot code

--- tf2_bot/commands/role.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'role',
-    description: 'Set a users role',
-    args: true,
-    usage: '<user> <role>',
-    execute(message, args) {
-        message.channel.send('no');
-    }
-}
