e89ff193ce548e29a486e0dd26f6fb948976ca15
peppizza <spencervess@gmail.com>
2020-07-05 12:18:23 -0400

    added basic logging

--- tf2_bot/index.js
+++ tf2_bot/index.js
@@ -1,8 +1,9 @@
-const { CommandoClient, SQLiteProvider} = require('discord.js-commando');
+const { CommandoClient, SQLiteProvider, FriendlyError } = require('discord.js-commando');
 const path = require('path');
 const { token } = require('./config.json');
 const sqlite = require('sqlite');
 const sqlite3 = require('sqlite3');
+const { oneLine } = require('common-tags');
 
 const client = new CommandoClient({
     commandPrefix: '?',
@@ -21,10 +22,45 @@ client.registry
     })
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-client.on('ready', () => {
-    console.log('Logged in');
-    client.user.setActivity('game');
-});
+client
+    .on('error', console.error)
+    .on('warn', console.warn)
+    .on('debug', console.log)
+    .on('ready', () => {
+        console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+    })
+    .on('disconnect', () => console.warn('Disconnected!'))
+    .on('reconnecting', () => console.warn('Reconnecting...'))
+    .on('commandError', (cmd, err) => {
+        if(err instanceof FriendlyError) return;
+        console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+    })
+    .on('commandBlock', (msg, reason) => {
+        console.log(oneLine`
+            Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+            blocked; ${reason}
+        `);
+    })
+    .on('commandPrefixChange', (guild, prefix) => {
+        console.log(oneLine`
+            Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
+            ${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}
+        `)
+    })
+    .on('commandStatusChange', (guild, command, enabled) => {
+        console.log(oneLine`
+            Command ${command.groupID}:${command.memberName}
+            ${enabled ? 'enabled' : 'disabled'}
+            ${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}
+        `);
+    })
+    .on('groupStatusChange', (guild, group, enabled) => {
+        console.log(oneLine`
+            Group ${group.id}
+            ${enabled ? 'enabled' : 'disabled'}
+            ${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}
+        `);
+    });
 
 sqlite.open( { filename: path.join(__dirname, 'settings.sqlite3'), driver: sqlite3.Database } ).then(db => {
     client.setProvider(new SQLiteProvider(db));
