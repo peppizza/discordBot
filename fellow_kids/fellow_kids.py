c88d39883616ce2a3c9c8bd5b219a893a7fd1578
peppizza <spencervess@gmail.com>
2020-07-05 13:54:59 -0400

    requirements file

--- fellow_kids/fellow_kids.py
+++ fellow_kids/fellow_kids.py
@@ -13,7 +13,7 @@ API_TOKEN = os.getenv('DISCORD_TOKEN')
 
 logging.basicConfig(level=logging.INFO)
 
-class bot(commands.AutoShardedBot):
+class FellowKids(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(command_prefix='!', owner_id=253290704384557057, reconnect=True, case_insensitive=False)
 
@@ -21,7 +21,6 @@ class bot(commands.AutoShardedBot):
         self.cursor = self.db.cursor()
 
         self.remove_command('help')
-
 
     async def on_ready(self):
         await self.change_presence(activity=discord.Game(name='DM me !report to report a user'))
@@ -33,4 +32,4 @@ class bot(commands.AutoShardedBot):
                 self.load_extension(f'commands.{name}')
 
 
-bot().run(API_TOKEN)
+FellowKids().run(API_TOKEN)
