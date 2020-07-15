d2d52a8ccbdeeece988205bd658a2f0b16ac1f83
peppizza <spencervess@gmail.com>
2020-07-15 17:28:09 -0400

    i think simple_commands is done

--- fellow_kids/commands/simple_commands.py
+++ fellow_kids/commands/simple_commands.py
@@ -19,31 +19,7 @@ class SimpleCommands(commands.Cog):
     async def kids(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=bI2PdskxE_s')
 
-    @commands.command(help='pootis')
-    async def pootis(self, ctx):
-        await ctx.send('pootis')
-
-    @commands.command(help='jesus')
-    async def jesus(self, ctx):
-        await ctx.send('https://www.you-need-jesus.com/')
-
-    @commands.command(help='your mom be a hoe')
-    async def momsahoe(self, ctx):
-        await ctx.send('https://www.youtube.com/watch?v=iYx_aGlmyc4')
-
-    @commands.command(help='heyayayaya')
-    async def hey(self, ctx):
-        await ctx.send('https://www.youtube.com/watch?v=ZZ5LpwO-An4&t=13s')
-
-    @commands.command(help='shrek')
-    async def shrek(self, ctx):
-        await ctx.send('https://www.youtube.com/watch?v=psFzJv8g6jc')
-
-    @commands.command(help='hamburger')
-    async def hamburger(self, ctx):
-        await ctx.send('https://www.youtube.com/watch?v=qIJ-lchwqVA')
-
-    @commands.command(help='selects random tf2 image')
+    @commands.command(help='Selects random TF2 image')
     async def tf2image(self, ctx):
         await ctx.send(random.choice(self.tf2images))
 
@@ -51,15 +27,16 @@ class SimpleCommands(commands.Cog):
     async def nou(self, ctx):
         await ctx.send('https://cdn.discordapp.com/attachments/684474004563689539/685130037585772552/deepfried_1583418116265.png')
 
+    # idk about this one -bagguette
     @commands.command(help='gay')
     async def gay(self, ctx):
         await ctx.send('https://www.villagevoice.com/wp-content/uploads/2011/02/thatsgay.png')
 
-    @commands.command(help='kool aid man')
+    @commands.command(help='Kool aid man doing the sexing ( ͡° ͜ʖ ͡°)')
     async def ohyeah(self, ctx):
         await ctx.send('https://i.pinimg.com/736x/e2/d2/4a/e2d24a8338a81191c59b928c2cbeedcf.jpg')
 
-    @commands.command(help='deep')
+    @commands.command(help='It rlly is deep tho')
     async def deep(self, ctx):
         await ctx.send('https://preview.redd.it/she0nt0g5b131.jpg?auto=webp&s=e77d77c2bf39d54d9cb89457eee61220d5700df4')
 
@@ -71,15 +48,15 @@ class SimpleCommands(commands.Cog):
     async def bruh(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=2ZIpFytCSVc')
 
-    @commands.command(help='get vectored')
+    @commands.command(help='You just got vectored')
     async def vector(self, ctx):
         await ctx.send('https://cdn.discordapp.com/attachments/685262422252191781/685591126442377266/download.jpg')
 
-    @commands.command(help='gonk image')
+    @commands.command(help='Shows the gonk image')
     async def gonk(self, ctx):
         await ctx.send('https://cdn.discordapp.com/attachments/685262422252191781/685621651970326545/unknown.png')
 
-    @commands.command(help='prints smug anime girl')
+    @commands.command(help='Shows a random smug anime girl')
     async def smug(self, ctx):
         await ctx.send(random.choice(self.smuganime))
 
@@ -87,20 +64,21 @@ class SimpleCommands(commands.Cog):
     async def didntseethat(self, ctx):
         await ctx.send('https://cdn.discordapp.com/attachments/685262422252191781/689161554318721042/maxresdefault.jpg')
 
-    @commands.command()
+    @commands.command(help='Spycrabs but in vagina')
     async def spycrab(self, ctx):
         await ctx.send('https://i.imgur.com/ufzaw81.png')
 
-    @commands.command()
+    @commands.command(help='Why are you gay')
     async def whygay(self, ctx):
         await ctx.send('https://cdn.discordapp.com/attachments/697102775959552052/704357844572438598/images.jpg')
 
-    @commands.command()
+    @commands.command(help='POV monke')
     async def monke(self, ctx):
         await ctx.send('https://i.redd.it/97vk2gz7zvl41.jpg')
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
+        """Adds a reaction to a new reaction within 1 to 3 seconds"""
         if reaction.count != 1: return
         message = reaction.message
         emoji = reaction.emoji
