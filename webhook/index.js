ae0778ee5b2cac4ee944a32ca38e2839131511eb
peppizza <spencervess@gmail.com>
2020-08-13 19:19:58 -0400

    i guess prettier wants this

--- webhook/index.js
+++ webhook/index.js
@@ -10,10 +10,10 @@ const PORT = 4567;
 app.use(express.json());
 
 app.post("/payload", async (req, res) => {
-	res.send();
-	if (req.body.ref === "refs/heads/master") {
-		await git.pull("origin", "master");
-	}
+  res.send();
+  if (req.body.ref === "refs/heads/master") {
+    await git.pull("origin", "master");
+  }
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
